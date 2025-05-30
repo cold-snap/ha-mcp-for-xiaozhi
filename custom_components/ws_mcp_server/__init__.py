eaecb401ecfd2a4eedd8944420a7c61129ffff9c
c1pher <c1pher-cn@users.noreply.github.com>
2025-05-30 23:25:55 +0800

    Update __init__.py

--- custom_components/ws_mcp_server/__init__.py
+++ custom_components/ws_mcp_server/__init__.py
@@ -2,7 +2,7 @@
 """The Model Context Protocol Server integration."""
 
 from __future__ import annotations
-
+from homeassistant.const import EVENT_HOMEASSISTANT_STARTED
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.typing import ConfigType
@@ -31,12 +31,21 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 
+#async def async_setup_entry(hass: HomeAssistant, entry: WsMCPServerConfigEntry) -> bool:
+#    """Set up Model Context Protocol Server from a config entry."""
+#    entry.runtime_data = SessionManager()
+#    await websocket_transport.async_setup_entry(hass,entry)
+#    return True
+    
 async def async_setup_entry(hass: HomeAssistant, entry: WsMCPServerConfigEntry) -> bool:
     """Set up Model Context Protocol Server from a config entry."""
-    entry.runtime_data = SessionManager()
-    await websocket_transport.async_setup_entry(hass,entry)
+    async def _system_started(event):
+        entry.runtime_data = SessionManager()
+        await websocket_transport.async_setup_entry(hass,entry)
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STARTED, _system_started)
     return True
-#await websocket_transport.async_setup_entry(hass, entry)
+    
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: WsMCPServerConfigEntry) -> bool:
